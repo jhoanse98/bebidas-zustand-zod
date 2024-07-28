cefeb5771a65a68670783ad20697f79ca16c9fca
Sebas24 <sebastian.marin@lernit.com.mx>
2024-07-28 17:11:38 -0500

    feat: se agrega la imagen del header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,15 @@
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 import { NavLink, useLocation } from "react-router-dom";
+import { useAppStore } from "../stores/useAppStore";
 
 const Header = () => {
   const { pathname } = useLocation();
   const isHome = useMemo(() => pathname === "/", [pathname]);
 
+  const fetchCategories = useAppStore((state) => state.fetchCategories);
+  useEffect(() => {
+    fetchCategories();
+  }, []);
   return (
     <header className={isHome ? "bg-header" : "bg-slate-800"}>
       <div className="mx-auto container px-5 py-16">
