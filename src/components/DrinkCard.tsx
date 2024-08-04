a499628fd3c32a97dc154b55df5e96be8e03feec
Sebas24 <sebastian.marin@lernit.com.mx>
2024-08-04 17:58:57 -0500

    se eliminan importaciones de react

--- src/components/DrinkCard.tsx
+++ src/components/DrinkCard.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Drink } from "../types";
 import { useAppStore } from "../stores/useAppStore";
 
--- src/pages/FavoritePage.tsx
+++ src/pages/FavoritePage.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import { useMemo } from "react";
 import { useAppStore } from "../stores/useAppStore";
 import DrinkCard from "../components/DrinkCard";
 
