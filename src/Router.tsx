abc3f9881d03db18c1606032f5a117ca3e7173a7
Sebas24 <sebastian.marin@lernit.com.mx>
2024-08-04 18:08:26 -0500

    se usa lazy loading para un build liviano

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,32 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import IndexPage from "./pages/IndexPage";
 import Layout from "./layouts/Layout";
-import FavoritePage from "./pages/FavoritePage";
+
+const IndexPage = lazy(() => import("./pages/IndexPage"));
+const FavoritePage = lazy(() => import("./pages/FavoritePage"));
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<IndexPage />} index />
-          <Route path="/favoritos" element={<FavoritePage />} />
+          <Route
+            path="/"
+            element={
+              <Suspense fallback="Cargando...">
+                <IndexPage />
+              </Suspense>
+            }
+            index
+          />
+          <Route
+            path="/favoritos"
+            element={
+              <Suspense fallback="Cargando...">
+                <FavoritePage />
+              </Suspense>
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
